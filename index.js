9f2728c5060ab36db1be3f0a138c16208062b50f
Max Fechner <max.fechner@gmail.com>
2022-01-21 10:28:30 +0100

    ...

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 console.log(Deno.version)
 // import { loadAndConvert } from './mod.bundle.js'
 
+// test:
+// https://xlsx.deno.dev/?sheet=D_2016_2021_KW_AG_Ins&url=https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Sterbefaelle-Lebenserwartung/Tabellen/sonderauswertung-sterbefaelle.xlsx?__blob=publicationFile
+
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 serve(main)
 
 
 import { loadAndConvert } from './mod.js'
+
 async function main(request) {
 	let url = new URL(request.url)
 	// let path = url.pathname;
 	let params = Object.fromEntries(url.searchParams)
+	if (!params.url) { return null }
 	console.log('sheet', params.sheet)
 	console.log('url', params.url)
-	if (!params.url) { return null }
 
 	let output = await loadAndConvert(params.url)
-	console.log(output)
+	console.log(Object.keys(output ?? {}))
 	// let text = Object.entries(output).map(x=>x[0]join('\n\n\n')
 	let text = output[params.sheet]
 	return new Response(text, {
